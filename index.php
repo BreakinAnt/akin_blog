c1ec31bf48a8746ed682a8fec1c644b04efbef6e
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-08-24 12:47:46 -0300

    Refactor index.php to initialize Database instance and define global db function

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Database;
 use App\Providers\RouteServiceProvider;
 
 try {
     require "autoloader.php";
     require "helper.php";
     require "Database.php";
+    $db = new Database();
+    function db() {
+        global $db;
+        return $db;
+    }
     require "Response.php";
     require "Router.php";
 
