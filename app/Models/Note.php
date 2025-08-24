2af5cf860ea2de9e2fd4cfabeca2042fe139c8e2
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-08-24 14:27:55 -0300

    Add Note and User model classes with relationships

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,5 +2,10 @@
 namespace App\Models;
 
 class Note extends Model {
+    protected $table = 'notes';
+    public $id, $content, $user_id;
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
