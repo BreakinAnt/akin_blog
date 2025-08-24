36cfb636e08e7dc0a5be49ff6d0189faff995ced
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-08-24 14:33:06 -0300

    Refactor Model class to streamline database interactions and improve method structure

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,20 +1,23 @@
 <?php
 namespace App\Models;
 
+use App\Database;
+
 class Model {
-    protected $table, $instance, $db;
+    protected $table, $db;
 
-    public function __construct() {
-        $this->instance = new static();
-        $this->db = db();
-    }
-
-    public function getTable()
+    public function __construct() 
     {
-        return $this->table !== null ? $this->table : strtolower((new \ReflectionClass(static::class))->getShortName()).'s';
+        $this->db = (new Database())->from($this->getTable());
+        $this->table = $this->getTable();
     }
 
-    public static function all()
+    public function getTable(): String
+    {
+        return $this->table ?? strtolower((new \ReflectionClass(static::class))->getShortName()).'s';
+    }
+
+    public static function all(): Array
     {
         $instance = new static();
         $instance->getTable();
@@ -22,10 +25,31 @@ class Model {
         return db()->from($instance->getTable())->get();
     }
 
-    public function where($column, $value, $comparator = '=')
+    public function where($column, $value, $comparator = '='): Model
     {
-        dd($this->instance);
-        return db()->from($instance->getTable())->where($column, $comparator, $value)->get();
+        $this->db->where($column, $comparator, $value);
+        return $this;
+    }
+
+    public function first(): Model
+    {
+        $data = $this->db->get()[0];
+        $this->setAttributes($data);
+        return $this;
+    }
+
+    private function setAttributes($attributes): void
+    {
+        foreach($attributes as $key => $value) {
+            $this->$key = $value;
+        }
     }
     
+    protected function belongsTo($related, $foreignKey = null, $localKey = 'id'): Model
+    {
+        $related = new $related();
+        $foreignKey = $foreignKey ?? strtolower((new \ReflectionClass($related))->getShortName()).'_id';
+
+        return $related->where($localKey, $this->$foreignKey)->first();
+    }
 }
