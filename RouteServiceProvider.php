50fa5174d872936bbf019e9d459932ade9be0573
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-08-23 22:55:51 -0300

    Refactor boot method in RouteServiceProvider to handle unmatched routes and return early instead of aborting with a 404 response.

--- RouteServiceProvider.php
+++ RouteServiceProvider.php
@@ -2,13 +2,42 @@
 
 class RouteServiceProvider
 {
-    static function boot()
+    static function boot(): void
     {
         $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
         $routes = require('routes.php');
 
         if(!isset($routes[$uri])){
-            abort(Response::NOT_FOUND, 'Page Not Found');
+            foreach($routes as $path => $route) {
+
+                preg_match_all('/\/{.*?\}/', $path, $slugs);
+                $slugs = $slugs[0];
+                
+                $cleanPath = str_replace($slugs, '', $path);
+
+                preg_match_all('/\\/[^\\/]+/', str_replace($cleanPath, '', $uri), $uriSlugs);
+                $uriSlugs = $uriSlugs[0];
+
+                $cleanUri = str_replace($uriSlugs, '', $uri);
+
+                $uriAndPathIsEqual = $cleanUri === $cleanPath;
+
+                if(!$uriAndPathIsEqual) {
+                    continue;
+                }
+
+                if(count($slugs) !== count($uriSlugs)) {
+                    continue;
+                }
+
+                $controller = $route['controller'];
+                $renderFunction = $route['renderFunction'];
+
+                $controller->$renderFunction();
+                return;
+            }
+
+            return;
         }
 
         $route = $routes[$uri];
