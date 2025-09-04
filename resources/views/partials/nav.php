a9fc8df1fa22afd5b36c33da2fef9025638aca0a
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-09-03 22:06:58 -0300

    WIP front design

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,6 +1,29 @@
-<nav>
-  <a href="/" <?php echo urlIs("/") ? $activeClass : $inactiveClass ?> >Home</a>
-  <a href="/about" <?php echo urlIs("/about") ? $activeClass : $inactiveClass ?> >About</a>
-  <a href="/notes" <?php echo urlIs("/notes") ? $activeClass : $inactiveClass ?> >Notes</a>
-  <a href="/contact" <?php echo urlIs("/contact") ? $activeClass : $inactiveClass ?> >Contact</a>
+<nav class="no-select">
+  <div class="nav-button">
+    <div class="nav-button-text">Home
+      <div class="nav-button-line"></div>
+    </div>
+  </div>
+  <div class="nav-button">
+    <div class="nav-button-text">About
+      <div class="nav-button-line"></div>
+    </div>
+  </div>
+  <div class="nav-button">
+    <div class="nav-button-text">Notes
+      <div class="nav-button-line"></div>
+    </div>
+  </div>
+  <div class="nav-button">
+    <div class="nav-button-text">Contact
+      <div class="nav-button-line"></div>
+    </div>
+  </div>
+  <!-- <a href="/" class="<?php echo urlIs("/") ? 'url-active' : 'url-inactive' ?>">
+    <p>Home</p>
+    <div>#</div>
+  </a>
+  <a href="/about" class="<?php echo urlIs("/about") ? 'url-active' : 'url-inactive' ?>" >About</a>
+  <a href="/notes" class="<?php echo urlIs("/notes") ? 'url-active' : 'url-inactive' ?>" >Notes</a>
+  <a href="/contact" class="<?php echo urlIs("/contact") ? 'url-active' : 'url-inactive' ?>" >Contact</a> -->
 </nav>
