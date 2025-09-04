60c812a283c220e08eac01bb7e69217d97728a19
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-09-03 22:31:23 -0300

    Refactor navigation links to use anchor tags for better accessibility and styling

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,29 +1,26 @@
 <nav class="no-select">
   <div class="nav-button">
-    <div class="nav-button-text">Home
+    <div class="nav-button-text">
+      <a href="/" class="<?php echo urlIs("/") ? 'url-active' : 'url-inactive' ?>">Home</a>
       <div class="nav-button-line"></div>
     </div>
   </div>
   <div class="nav-button">
-    <div class="nav-button-text">About
+    <div class="nav-button-text">
+      <a href="/about" class="<?php echo urlIs("/about") ? 'url-active' : 'url-inactive' ?>" >About</a>
       <div class="nav-button-line"></div>
     </div>
   </div>
   <div class="nav-button">
-    <div class="nav-button-text">Notes
+    <div class="nav-button-text">
+      <a href="/notes" class="<?php echo urlIs("/notes") ? 'url-active' : 'url-inactive' ?>" >Notes</a>
       <div class="nav-button-line"></div>
     </div>
   </div>
   <div class="nav-button">
-    <div class="nav-button-text">Contact
+    <div class="nav-button-text">
+      <a href="/contact" class="<?php echo urlIs("/contact") ? 'url-active' : 'url-inactive' ?>" >Contact</a>
       <div class="nav-button-line"></div>
     </div>
   </div>
-  <!-- <a href="/" class="<?php echo urlIs("/") ? 'url-active' : 'url-inactive' ?>">
-    <p>Home</p>
-    <div>#</div>
-  </a>
-  <a href="/about" class="<?php echo urlIs("/about") ? 'url-active' : 'url-inactive' ?>" >About</a>
-  <a href="/notes" class="<?php echo urlIs("/notes") ? 'url-active' : 'url-inactive' ?>" >Notes</a>
-  <a href="/contact" class="<?php echo urlIs("/contact") ? 'url-active' : 'url-inactive' ?>" >Contact</a> -->
 </nav>
