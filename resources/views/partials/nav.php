b97211f206a54cb37b36f35a0e4f84e45a32849b
Felipe <felipesan08@hotmail.com>
2025-09-10 19:11:19 -0300

    Refactor navigation links to consistently use urlIs function with active/inactive classes for improved readability

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -1,26 +1,27 @@
+
 <nav class="no-select">
   <div class="nav-button">
     <div class="nav-button-text">
-      <a href="/" class="<?php echo urlIs("/") ? 'url-active' : 'url-inactive' ?>">Home</a>
-      <div class="nav-button-line"></div>
+      <a href="/" class="<?php echo urlIs("/", 'url-active', 'url-inactive') ?>">Home</a>
+      <div class="nav-button-line <?php echo urlIs("/", 'url-active', 'url-inactive') ?>"></div>
     </div>
   </div>
   <div class="nav-button">
     <div class="nav-button-text">
-      <a href="/about" class="<?php echo urlIs("/about") ? 'url-active' : 'url-inactive' ?>" >About</a>
-      <div class="nav-button-line"></div>
+      <a href="/about" class="<?php echo urlIs("/about", 'url-active', 'url-inactive') ?>" >About</a>
+      <div class="nav-button-line <?php echo urlIs("/about", 'url-active', 'url-inactive') ?>"></div>
     </div>
   </div>
   <div class="nav-button">
     <div class="nav-button-text">
-      <a href="/notes" class="<?php echo urlIs("/notes") ? 'url-active' : 'url-inactive' ?>" >Notes</a>
-      <div class="nav-button-line"></div>
+      <a href="/notes" class="<?php echo urlIs("/notes", 'url-active', 'url-inactive') ?>" >Notes</a>
+      <div class="nav-button-line <?php echo urlIs("/notes", 'url-active', 'url-inactive') ?>"></div>
     </div>
   </div>
   <div class="nav-button">
     <div class="nav-button-text">
-      <a href="/contact" class="<?php echo urlIs("/contact") ? 'url-active' : 'url-inactive' ?>" >Contact</a>
-      <div class="nav-button-line"></div>
+      <a href="/contact" class="<?php echo urlIs("/contact", 'url-active', 'url-inactive') ?>" >Contact</a>
+      <div class="nav-button-line <?php echo urlIs("/contact", 'url-active', 'url-inactive') ?>"></div>
     </div>
   </div>
 </nav>
