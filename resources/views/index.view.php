beaa1e12f9a8babea3a80e4ea4c5329416dc2216
Felipe <felipesan08@hotmail.com>
2025-09-10 19:49:49 -0300

    Refactor index view layout and restore image display for posts

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -1,15 +1,13 @@
 <?php require("resources/views/partials/head.php") ?>
 <?php require("resources/views/partials/nav.php") ?>
 
-<main class="bg-primary">
-  <div>
-    <p><?= $heading ?></p>
-  </div>
+<main class="border-end border-start border-black">
   <ul>
     <?php foreach($posts as $post): ?>
       <li>
         <article>
           <h2><?= $post['title'] ?></h2>
+          <img src="<?= getImage($post['banner_path']) ?>" style="max-width:300px;">
           <p><?= formatDate($post['date']) ?></p>
           <p><?= trimText($post['content']) ?></p>
         </article>
