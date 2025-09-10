8f1d880c97b4b6e5414c07b1fc1896450f6fcff3
Felipe <felipesan08@hotmail.com>
2025-09-10 20:34:35 -0300

    Enhance post display layout and styling in index view for improved readability

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -1,19 +1,21 @@
 <?php require("resources/views/partials/head.php") ?>
 <?php require("resources/views/partials/nav.php") ?>
 
-<main class="border-end border-start border-secondary">
-  <ul>
-    <?php foreach($posts as $post): ?>
-      <li>
-        <article>
+<main class="border-end border-start border-secondary p-5">
+  <?php foreach($posts as $post): ?>
+      <article class="post-list">
+        <div class="post-header">
+          <img class="post-banner" src="<?= getImage($post['banner_path']) ?>">
+        </div>
+        <div class="post-content">
           <h2><?= $post['title'] ?></h2>
-          <img src="<?= getImage($post['banner_path']) ?>" style="max-width:300px;">
-          <p><?= formatDate($post['date']) ?></p>
+          <span><?= formatDate($post['date']) ?></span>
+          <span class="text-black">Technology</span>
           <p><?= trimText($post['content']) ?></p>
-        </article>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+          <a href="/post/<?= $post['id'] ?>"><span>Read more</span> <iconify-icon icon="solar:alt-arrow-right-line-duotone" width="20" height="20"></iconify-icon> </a>
+        </div>
+      </article>
+  <?php endforeach; ?>
 </main>
 
 <?php require("resources/views/partials/footer.php") ?>
--- resources/views/partials/footer.php
+++ resources/views/partials/footer.php
@@ -5,5 +5,6 @@
     </footer>
     </div>
     <script src="/resources/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@3.0.0/dist/iconify-icon.min.js"></script>
 </body>
 </html>
