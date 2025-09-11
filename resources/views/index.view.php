8ed986deafe654ea64677dfcdbe37c2f3386157e
Felipe <felipesan08@hotmail.com>
2025-09-10 21:22:00 -0300

    Refactor post display in index view to enhance metadata presentation and improve layout

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -9,10 +9,22 @@
         </div>
         <div class="post-content">
           <h2><?= $post['title'] ?></h2>
-          <span><?= formatDate($post['date']) ?></span>
-          <span class="text-black">Technology</span>
-          <p><?= trimText($post['content']) ?></p>
-          <a href="/post/<?= $post['id'] ?>"><span>Read more</span> <iconify-icon icon="solar:alt-arrow-right-line-duotone" width="20" height="20"></iconify-icon> </a>
+          <div class="post-meta">
+            <div class="post-meta-item">
+              <span>
+                <iconify-icon icon="solar:calendar-linear" width="15" height="15"></iconify-icon> 
+                <?= formatDate($post['date']) ?>
+              </span>
+            </div>
+            <div class="post-meta-item">
+              <a href="/post/<?= $post['slug'] ?>">
+                <iconify-icon icon="solar:folder-linear" width="15" height="15"></iconify-icon> 
+                Technology
+              </a>
+            </div>
+          </div>
+          <p><?= trimText($post['description'], 255) ?></p>
+          <a class="post-read-more" href="/post/<?= $post['slug'] ?>">Read more <iconify-icon icon="solar:alt-arrow-right-line-duotone" width="20" height="20"></iconify-icon> </a>
         </div>
       </article>
   <?php endforeach; ?>
