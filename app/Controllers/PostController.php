eb005ffcdae698a97b3adfe40e01e56c2de2db85
Felipe <felipesan08@hotmail.com>
2025-09-10 23:11:35 -0300

    Add view_count property to Post model and update view count in PostController

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 
+use App\Database;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -10,6 +11,9 @@ class PostController extends Controller
         $heading = 'Post';
 
         $post = (new Post())->where('slug', $noteSlug)->first();
+        
+        #TODO: Change to increment view count in model save method
+        (new Database())->raw("UPDATE posts SET view_count = ? WHERE id = $post->id", [$post->view_count+1]);
 
         $this->render('posts/show', compact('heading', 'post'));
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,6 @@
 namespace App\Models;
 
 class Post extends Model {
-    public $id, $slug, $title, $description, $content, $date, $hidden, $banner_path;
+    public $id, $slug, $title, $description, $content, $date, $hidden, $banner_path, $view_count;
     
 }
