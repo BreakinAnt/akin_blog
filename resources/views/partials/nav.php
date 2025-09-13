fc54b9346fd0dc37b17401d299f8536946a35025
Felipe <felipesan08@hotmail.com>
2025-09-13 00:06:13 -0300

    Add GitHub and LinkedIn links to navigation for improved accessibility

--- resources/views/partials/nav.php
+++ resources/views/partials/nav.php
@@ -14,6 +14,18 @@
   </div>
   <div class="nav-button">
     <div class="nav-button-text">
+      <a href="https://github.com/BreakinAnt/akin_blog" class="url-inactive" >Git</a>
+      <div class="nav-button-line url-inactive"></div>
+    </div>
+  </div>
+  <div class="nav-button">
+    <div class="nav-button-text">
+      <a href="https://www.linkedin.com/in/felipesan08/" class="url-inactive" >LinkedIn</a>
+      <div class="nav-button-line url-inactive"></div>
+    </div>
+  </div>
+  <!-- <div class="nav-button">
+    <div class="nav-button-text">
       <a href="/notes" class="<?php echo urlIs("/notes", 'url-active', 'url-inactive') ?>" >Notes</a>
       <div class="nav-button-line <?php echo urlIs("/notes", 'url-active', 'url-inactive') ?>"></div>
     </div>
@@ -23,5 +35,5 @@
       <a href="/contact" class="<?php echo urlIs("/contact", 'url-active', 'url-inactive') ?>" >Contact</a>
       <div class="nav-button-line <?php echo urlIs("/contact", 'url-active', 'url-inactive') ?>"></div>
     </div>
-  </div>
+  </div> -->
 </nav>
