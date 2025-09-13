fd5bc54a49b9c5092f1990e0694ed054e114801b
Felipe <felipesan08@hotmail.com>
2025-09-13 00:06:09 -0300

    Add about page styling and content for improved presentation

--- resources/views/about.view.php
+++ resources/views/about.view.php
@@ -1,10 +1,17 @@
 <?php require("resources/views/partials/head.php") ?>
 <?php require("resources/views/partials/nav.php") ?>
 
-<main>
-  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <p><?= $heading ?></p>
-  </div>
+<main class="about-page">
+    <div>
+        <img class="about-logo" src="../../public/storage/doggies/f.png" alt="Kinu.blog Logo">
+    </div>
+    <div class="about-content">
+        <p>Kinu.blog (Akin Blog) is a small blog engine built in pure PHP. No frameworks, no heavy setup. Just a simple MVC structure with routing, controllers, models, and a MySQL database.</p>
+
+        <p>The main goal right now is to use this as a study project. I want to experiment with PHP concepts and patterns by building things from scratch, without the abstractions that frameworks usually give you. At the same time, the plan is to keep improving it and eventually use it as my own personal blog.</p>
+
+        <p>Since this project is mostly for study purposes, especially for experimenting with more advanced PHP features and practical concepts that are usually hidden away when using big frameworks. You'll find things like type hints, return types, relationships in models, and some custom routing solutions. It's a place to try out ideas, experiment and see how various good code practices actually work when you put them together from scratch.</p>
+    </div>
 </main>
 
 <?php require("resources/views/partials/footer.php") ?>
