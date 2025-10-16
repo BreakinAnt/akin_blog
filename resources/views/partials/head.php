49f6399712bb64039120352bc507c6091bb07de2
Felipe <felipesan08@hotmail.com>
2025-10-15 22:32:35 -0300

    Update head.php to enhance title formatting and add favicon links for improved branding

--- resources/views/partials/head.php
+++ resources/views/partials/head.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kinu Blog <?= isset($heading) ? "- $heading" : '' ?></title>
+    <title><?= isset($heading) ? "$heading •" : '' ?> Kinu Blog</title>
     <link href="/resources/css/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="/resources/css/app.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/public/favicon_io/site.webmanifest">
 </head>
 
 <body>
